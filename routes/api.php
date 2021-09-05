8eb0868046b8d01bdfcbecd953d63f360d32ea40
go <govella08@gmail.com>
2021-09-06 01:56:29 +0300

    Added apiResource route for Jobs

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\JobController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('/categories', CategoryController::class);
+Route::apiResource('jobs', JobController::class);
